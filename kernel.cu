96d0054ae4dc563e439886c5c6cbb8bb76d28725
jamesratini <jratini@carthage.edu>
2017-01-16 11:43:38 -0600

    Add files via upload

--- kernel.cu
+++ kernel.cu
@@ -9,58 +9,165 @@
 
 using namespace cv;
 using namespace std;
+//GLOBALS
+int thresholdSlider;
+const int THRESHOLD_SLIDER_MAX = 255;
+cudaDeviceProp deviceProps;
+Mat hostImage;
+unsigned char THRESHOLD = 120;
+unsigned char *dev0_image;
+unsigned char *devCopy_image;
+int imageSize = 0;
+//GLOBALS
+__global__ void kernel(unsigned char* imageOrig, unsigned char* imageCopy, unsigned char threshold)
+{
+	
+	int j = blockIdx.x * blockDim.x + threadIdx.x;
+	
+	if (imageOrig[j] > threshold)
+	{
+		imageCopy[j] = 255;
+	}
+	else
+	{
+		imageCopy[j] = 0;
+	}
+}
 
 void Threshold(Mat image, unsigned char threshold);
+void thresholdWithCuda(Mat* image, unsigned char threshold);
+
+void on_Trackbar(int, void *)
+{
+	int blocksNeeded = (imageSize + deviceProps.maxThreadsPerBlock - 1) / deviceProps.maxThreadsPerBlock;
+	//use kernel to threshold dev0_image, then write to devCopy_image
+	kernel<<<blocksNeeded, deviceProps.maxThreadsPerBlock>>>(dev0_image, devCopy_image, thresholdSlider);
+	cudaDeviceSynchronize();
+
+	if (cudaMemcpy(hostImage.data, devCopy_image, imageSize, cudaMemcpyDeviceToHost) != cudaSuccess)
+	{
+		throw("trackbar memcopy failed");
+	}
+
+	imshow("Display window", hostImage);
+}
 
 int main(int argc, char** argv)
 {
+	
 	//if 2 arguements arent passed, tell user there was an error
 	if (argc != 2){
 		cout << "Usage: display_image ImageToLoadAndDisplay" << endl;
 		return -1;
 	}
 
-	//create Mat and attempt to read image into it from argv[1]
-	Mat image;
-	image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+	//set hostImage to command argument
+	hostImage = imread(argv[1], CV_LOAD_IMAGE_COLOR);
 
-	if (!image.data) {
+	if (!hostImage.data) {
 		cout << "Could not open or find the image" << endl;
 		return -1;
 	}
 	
-	cout << "Image has: " << image.channels() << " channels" << endl;
-	cout << "Image is " << image.cols << "x" << image.rows << endl;
+	//display image information
+	cout << "Image has: " << hostImage.channels() << " channels" << endl;
+	cout << "Image is " << hostImage.cols << "x" << hostImage.rows << endl;
 
-	cvtColor(image, image, cv::COLOR_RGB2GRAY);
-	cout << "Converted to gray" << endl;
+	//convert image to grayscale
+	cvtColor(hostImage, hostImage, cv::COLOR_RGB2GRAY);
 
-	unsigned char THRESHOLD = 200;
-	Threshold(image, THRESHOLD);
+	//time cpu threshold
+	//Threshold(hostImage, THRESHOLD);
+	//end time for cpu
 
-	namedWindow("Dispaly window", WINDOW_NORMAL);
-	imshow("Display window", image);
+	thresholdWithCuda(&hostImage, THRESHOLD);
 
-	//resizeWindow("Display window", 1920, 1080);
+	namedWindow("Display window", WINDOW_NORMAL);
+	resizeWindow("Display window", 1000, 900);
+	createTrackbar("Threshold", "Display window", &thresholdSlider, THRESHOLD_SLIDER_MAX, on_Trackbar);
+	imshow("Display window", hostImage);
 
 	waitKey(0);
 	return 0;
 }
 
-void Threshold(Mat image, unsigned char threshold)
+void Threshold(Mat hostImage, unsigned char threshold)
 {
-	int height = image.rows;
-	int width = image.cols;
+	int height = hostImage.rows;
+	int width = hostImage.cols;
 
 	for (int i = 0; i < height*width; i++)
 	{
-		if (image.data[i] > threshold)
+		if (hostImage.data[i] > threshold)
 		{
-			image.data[i] = 255;
+			hostImage.data[i] = 255;
 		}
 		else
 		{
-			image.data[i] = 0;
+			hostImage.data[i] = 0;
 		}
 	}
+}
+void thresholdWithCuda(Mat* hostImage, unsigned char threshold)
+{
+	cudaError_t cudaStatus;
+	
+	int height = hostImage->rows;
+	int width = hostImage->cols;
+	imageSize = height * width;
+	
+	try
+	{
+		//set device
+		cudaStatus = cudaSetDevice(0);
+		if (cudaStatus != cudaSuccess)
+		{
+			throw("error in set device");
+		}
+		//check device props
+		cudaStatus = cudaGetDeviceProperties(&deviceProps, 0);
+		if (cudaStatus != cudaSuccess) {
+			throw("getDeviceProperties failed");
+		}
+		//malloc original image and copy image
+		cudaStatus = cudaMalloc((void**)&dev0_image, imageSize);
+		if (cudaStatus != cudaSuccess)
+		{
+			throw("cudaMalloc failed");
+		}
+
+		cudaStatus = cudaMalloc((void**)&devCopy_image, imageSize);
+		if (cudaStatus != cudaSuccess)
+		{
+			throw("cudaMalloc failed");
+		}
+
+		//copy original to gpu
+		cudaStatus = cudaMemcpy(dev0_image, hostImage->data, imageSize, cudaMemcpyHostToDevice);
+		if (cudaStatus != cudaSuccess)
+		{
+			throw("mem copy failed");
+		}
+		
+		int blocksNeeded = (imageSize + deviceProps.maxThreadsPerBlock - 1) / deviceProps.maxThreadsPerBlock;
+		//use kernel to threshold dev0_image, then write to devCopy_image
+		kernel<<<blocksNeeded, deviceProps.maxThreadsPerBlock>>>(dev0_image, devCopy_image, threshold);
+		if (cudaGetLastError() != cudaSuccess)
+			throw("add Kernel failed");
+		cudaStatus = cudaDeviceSynchronize();
+
+		cudaStatus = cudaMemcpy(hostImage->data, devCopy_image, imageSize, cudaMemcpyDeviceToHost);
+		if (cudaStatus != cudaSuccess)
+		{
+			throw("mem copy failed");
+		}
+	}
+	catch (char* error)
+	{
+		cout << error << endl;
+		goto bad_exit;
+	}
+bad_exit:
+	cudaFree((void*)&dev0_image);
+	cudaFree((void*)&devCopy_image);
 }
